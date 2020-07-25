7bb3e976cc233d5acc58963ae4835e7c3c4934fc
Ericanm <65022260+EricaNogueiraMendes@users.noreply.github.com>
2020-07-25 12:35:00 -0300

    alteracao na controller

--- DesafioSquad3/Controllers/WeatherForecastController.cs
+++ DesafioSquad3/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DesafioSquad3.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Verao", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
